39be33db246d4d38074e0208b0243928cc8e24da
suhyun <qkrtngus116@naver.com>
2023-03-09 08:57:22 +0900

    신규_프로그래머스_64065_튜플 -문자열 -substring, replace, 문자열을 split로 끊어 String[]배열에 담기, Arrays.sort(배열, new Comparator<String>(){ });

---
+++ GongCho/java/pg/230309_64065_튜플/Solution64065Test.java
@@ -0,0 +1,9 @@
+package Level2;
+
+public class Solution64065Test {
+    public static void main(String[] args){
+        Solution64065 s = new Solution64065();
+        String str = "{{1,2,3},{2,1},{1,2,4,3},{2}}";
+        System.out.println(s.solution(str));
+    }
+}
