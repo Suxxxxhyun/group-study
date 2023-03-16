bbd5497e4adb7c06c3adcca65b5b5032a47cc9c7
suhyun <qkrtngus116@naver.com>
2023-03-16 09:50:43 +0900

    복습_백준_20164_홀수홀리호석 -구현,완탐(성공) substring(i,j)는 i번째에서 j-1번째를 자르겠다는 의미이다.

--- GongCho/java/bj/230315_20164_홀수홀리호석/Main20164.java
+++ GongCho/java/bj/230315_20164_홀수홀리호석/Main20164.java
@@ -1,52 +1,53 @@
-package baekjun.src.baekjun.implement;
+package baekjun.simulation;
 
 import java.io.*;
+
 public class Main20164 {
+	
+	static int n, min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
 
-    static int n, min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        cut(n, getOdd(n));
-        System.out.println(min + " " + max);
-    }
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
+		cut(n, getOdd(n));
+		System.out.println(min + " " + max);
+	}
+	
+	static void cut(int x, int total) {
+		if(x < 10) {
+			min = Math.min(total, min);
+			max = Math.max(total, max);
+		} else if(x < 100) {
+			int sum = (x / 10) + (x % 10);
+			cut(sum, total + getOdd(sum));
+		} else {
+			String st = Integer.toString(x);
+			int len = st.length();
+			for(int i=0; i<=len-3; i++) {
+				for(int j=i+1; j<=len-2; j++) {
+					String s1 = st.substring(0,i+1);
+					String s2 = st.substring(i+1,j+1);
+					String s3 = st.substring(j+1,len);
+					
+					int sum = Integer.parseInt(s1) + Integer.parseInt(s2) + Integer.parseInt(s3);
+					
+					cut(sum, total + getOdd(sum));
+				}
+			}
+		}
+	}
+	
+	static int getOdd(int x) {
+		int cnt = 0;
+		while(x > 0) {
+			int val = x % 10;
+			if(val % 2 == 1) {
+				cnt++;
+			}
+			x /= 10;
+		}
+		return cnt;
+	}
 
-    static void cut(int x, int total){
-        if(x < 10){
-            min = Math.min(min, total);
-            max = Math.max(max, total);
-        } else if(x < 100){
-            int a = x / 10;
-            int b = x % 10;
-            cut(a + b, total + getOdd(a + b));
-        } else {
-            String st = Integer.toString(x);
-            int len = st.length();
-            for(int i=0; i<=len-3; i++){
-                for(int j=i+1; j<=len-2; j++){
-                    String s1 = st.substring(0, i+1);
-                    String s2 = st.substring(i+1, j+1);
-                    String s3 = st.substring(j+1, len);
-                    System.out.print(s1 + " " + s2 + " " + s3);
-                    System.out.println();
-
-                    int sum = Integer.parseInt(s1) + Integer.parseInt(s2) + Integer.parseInt(s3);
-                    cut(sum, total + getOdd(sum));
-                }
-            }
-        }
-    }
-
-    //입력의 최댓값이 10^9 - 1, 최대 9자리수, 따라서 getOdd메소드는 최대 9번까지 연산을 함.
-    static int getOdd(int n){
-        int cnt = 0;
-        while(n > 0){
-            int val = n % 10;
-            if(val % 2 == 1){
-                cnt++;
-            }
-            n /= 10;
-        }
-        return cnt;
-    }
 }
