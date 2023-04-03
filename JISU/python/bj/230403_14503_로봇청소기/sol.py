41f16f6e3f52b9b19278e476690d5cbe9e11a786
Jisoo Park <jisoo449@naver.com>
2023-04-03 17:45:39 +0900

    복습_백준_14503_로봇청소기
    -구현
    -map()함수를 사용한 입력 : map(int,input()) / map(int,input().split()) / list(map(int,input().split()))

--- JISU/python/bj/230403_14503_로봇청소기/sol.py
+++ JISU/python/bj/230403_14503_로봇청소기/sol.py
@@ -1,14 +1,18 @@
+#입력
 n,m=map(int,input().split())
 r,c,d=map(int,input().split())
 room=[list(map(int,input().split())) for _ in range(n)]
 move=[(-1,0),(0,1),(1,0),(0,-1)]
 
+
 while room[r][c]!='1':
+    #청소하면 변경(2)
     if room[r][c]=='0':
         room[r][c]='2'
 
     check=0
 
+    #반시계방향으로 체크
     for i in range(1,5):
         nd=d-i if d>=i else 4+(d-i)
         nr=r+move[nd][0]
@@ -18,11 +22,12 @@ while room[r][c]!='1':
             r,c,d=nr,nc,nd
             break
     
+    #빈곳이 없으면 후진
     if check==0:
         td=(d+2)%4
         r=r+move[td][0]
         c=c+move[td][1]
-        if not(0<=r<n and 0<=c<m):
+        if not(0<=r<n and 0<=c<m): #후진도 못하면 중단
             break
 
 
