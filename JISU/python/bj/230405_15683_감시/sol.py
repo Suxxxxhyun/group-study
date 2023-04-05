8034069e6c496d730e378e8e7b99184d6f916f76
Jisoo Park <jisoo449@naver.com>
2023-04-05 16:49:34 +0900

    복습_백준_15683_감시
    - dfs,bruteforce
    - 모든 경우의 수를 빠짐없이 체크해야 한다. 조건을 짤 때는 더 철저해지자

--- JISU/python/bj/230405_15683_감시/sol.py
+++ JISU/python/bj/230405_15683_감시/sol.py
@@ -1,50 +1,37 @@
 from copy import deepcopy
+import sys
+
+#입력 받기
 N,M=map(int,input().split())
 room=[list(map(int,input().split())) for _ in range(N)]
 
+#CCTV가 볼 수 있는 경우의 수
+cases={1:[[1],[2],[3],[4]], 2:[(1,3),(2,4)], 3:[(1,2),(2,3),(3,4),(4,1)], 4:[(1,2,3),(2,3,4),(3,4,1),(4,1,2)], 5:[(1,2,3,4)]}
+move=[(-1,0),(0,1),(1,0),(0,-1)]#북,동,남,서
+cctv=[(i,j) for j in range(M) for i in range(N) if room[i][j] in cases]#CCTV 좌표
+
+#CCTV가 보는대로 지도 변경
 def check(d,i,j,G):
-    if d==1:#북
-        for x in range(i-1,-1,-1):
-            if G[x][j]==0:G[x][j]='#'
-            elif G[x][j]==6:break
-    elif d==2:#동
-        for y in range(j+1,M):
-            if G[i][y]==0:G[i][y]='#'
-            elif G[i][y]==6:break
-    elif d==3:#남
-        for x in range(i+1,N):
-            if G[x][j]==0:G[x][j]='#'
-            elif G[x][j]==6:break
-    elif d==4:#서
-        for y in range(j-1,-1,-1):
-            if G[i][y]==0:G[i][y]='#'
-            elif G[i][y]==6:break
+    mv_i,mv_j=move[d-1][0],move[d-1][1]
+    while 0<=i<N and 0<=j<M and G[i][j]!=6:
+        if G[i][j]==0: G[i][j]='#'
+        i+=mv_i
+        j+=mv_j
 
-
-cases={1:[[1],[2],[3],[4]], 2:[(1,3),(2,4)], 3:[(1,2),(2,3),(3,4),(4,1)], 4:[(1,2,3),(2,3,4),(3,4,1)], 5:[(1,2,3,4)]}
-def blind_spot(i,j,directions,map):
-    for d in directions:
-        check(d,i,j,map)
+#사각지대 찾기(dfs+재귀+bruteforce)
+def blind_spot(cctv_idx,map):
+    if len(cctv)>0 and cctv_idx<len(cctv):
+        i,j=cctv[cctv_idx][0],cctv[cctv_idx][1]
+        nos=sys.maxsize
+        for direction in cases[map[i][j]]:
+            copied=deepcopy(map)
+            for d in direction:
+                check(d,i,j,copied)
+            nos=min(nos,blind_spot(cctv_idx+1,copied))
+        return nos
         
+    #사각지대 개수 계산
+    return sum([map[a].count(0) for a in range(N)])
 
-    for x in range(i,N):
-        start=0
-        if x==i:start=j+1
-        for y in range(start,M):
-            if map[x][y] in cases:
-                nos=[]
-                for case in cases[map[x][y]]:
-                    copied=deepcopy(map)
-                    nos.append(blind_spot(x,y,case,copied))
-                return min(nos)
-    
-    #사각지대 계산
-    answer=0
-    for a in range(N):
-        for b in range(M):
-            if map[a][b]==0:
-                answer+=1
-    
-    return answer
 
-print(blind_spot(0,-1,[],room))
+print(blind_spot(0,room))
