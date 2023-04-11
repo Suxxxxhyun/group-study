1115b87eccaa9cd08bae3e75cbaadee6156f2326
suhyun <qkrtngus116@naver.com>
2023-04-11 11:25:23 +0900

    신규_프로그래머스_159993_미로탈출 -bfs(실패) -그래프를 갱신하여 시간을 계산해주려고했더니, S에서 L로 갈때 E가 있을 수도 있고, L에서 E로 가려고 할때 S가 있을 수 도 있다. 따라서 처음에 생각한 방식(graph배열을 int배열로 바꾸어주고 L이나 E값을 특정음수로 표현)이 논리적으로 맞지 않아 Node클래스에 time변수를 새로 생성하여 시간을 계산해준다.

---
+++ GongCho/java/pg/2340411_159993_미로탈출/Solution159993Test.java
@@ -0,0 +1,11 @@
+package Level2;
+
+//1:45 ~ 2:15
+public class Solution159993Test {
+    public static void main(String[] args){
+        //Solution159993 s = new Solution159993();
+        String[] map = {"SOOOL","XXXXO","OOOOO","OXXXX","OOOOE"};
+        Solution15999302 s = new Solution15999302();
+        System.out.println(s.solution(map));
+    }
+}
