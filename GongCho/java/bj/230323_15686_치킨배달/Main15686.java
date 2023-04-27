fd8163f584cfb735ceca0903dd6b97aeef090cd5
suhyun <qkrtngus116@naver.com>
2023-04-28 08:22:03 +0900

    복습_백준_15686_치킨배달 -조합(성공)

--- GongCho/java/bj/230323_15686_치킨배달/Main15686.java
+++ GongCho/java/bj/230323_15686_치킨배달/Main15686.java
@@ -1,21 +1,18 @@
-package baekjun.DFSBFS;
+package baekjun.simulation;
 
+//7:50 ~ 8:20
 import java.io.*;
 import java.util.*;
-//바이러스 문제 다시 풀어보기
-//이전의 코드와 비교하면, 난 고른 치킨집을 selected에 넣어주었는데,
-//이전 코드는 selected배열 없이, 고른 치킨집을 isUsed에 true로 해주니,
-//isUsed가 true인 것을 토대로 계산해주면 됨.
 
 public class Main15686 {
 	
-	static int n,m, min = Integer.MAX_VALUE;
+	static int n,m, ans = Integer.MAX_VALUE;
 	static StringTokenizer st;
 	static int[][] map;
-	static ArrayList<Node> chicken = new ArrayList<Node>();
-	static ArrayList<Node> house = new ArrayList<Node>();
-	static Node[] selected;
-	static boolean[] isUsed;
+	static ArrayList<Node> chickens = new ArrayList<Node>();
+	static ArrayList<Node> houses = new ArrayList<Node>();
+	//고른 치킨집을 담는 배열
+	static int[] choose;
 	
 	static class Node{
 		int x, y;
@@ -23,10 +20,6 @@ public class Main15686 {
 		public Node(int x, int y) {
 			this.x = x;
 			this.y = y;
-		}
-		
-		public String toString() {
-			return "x = " + x + ", y = " + y;
 		}
 	}
 
@@ -36,58 +29,56 @@ public class Main15686 {
 		st = new StringTokenizer(br.readLine(), " ");
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
-		map = new int[n+1][n+1];
-		for(int i=1; i<=n; i++) {
+		map = new int[n][n];
+		for(int i=0; i<n; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
-			for(int j=1; j<=n; j++) {
-				int a = Integer.parseInt(st.nextToken());
-				map[i][j] = a;
-				if(a == 2) {
-					chicken.add(new Node(i,j));
-				}
-				if(a == 1) {
-					house.add(new Node(i,j));
+			for(int j=0; j<n; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+				if(map[i][j] == 2) {
+					chickens.add(new Node(i,j));
+				} else if(map[i][j] == 1) {
+					houses.add(new Node(i,j));
 				}
 			}
 		}
-		
-		isUsed = new boolean[chicken.size()];
-		selected = new Node[m];
-		
-		pro(0,0);
-		System.out.println(min);
-
+		//치킨집자체를 고를때, 중복을 허용하지 않으면서, 순서를 고려하지 않는 조합에 해당함.
+		choose = new int[m];
+		pro(0);
+		System.out.println(ans);
 	}
 	
-	//치킨 집을 고르는 메서드
-	static void pro(int idx, int cnt) {
-		if(cnt == m) {
-			int sum = 0;
-			for(int i=0; i<house.size(); i++) {
-				int dist = Integer.MAX_VALUE;
-				for(int j=0; j<selected.length; j++) {
-					if(selected[j] != null) {
-						int distance = Math.abs(house.get(i).x - selected[j].x) + Math.abs(house.get(i).y - selected[j].y);
-						dist = Math.min(distance, dist);
-					}
-				}
-				sum += dist;
-			}
-			//sum += dist;
-			min = Math.min(sum, min);
-			//System.out.println("min = " + min);
+	static void pro(int idx) {
+		
+		if(idx == m) {
+			//거리구하는 메소드
+			ans = Math.min(ans,solution());
 			return;
 		}
 		
-		
-		for(int i=idx; i<chicken.size(); i++) {
-			if(!isUsed[i]) {
-				selected[idx] = new Node(chicken.get(i).x, chicken.get(i).y);
-				isUsed[i] = true;
-				pro(i + 1, cnt + 1);
-				isUsed[i] = false;
-			}
+		int start = 0;
+		if(idx != 0) {
+			start = choose[idx - 1] + 1;
 		}
+		
+		for(int cand=start; cand<chickens.size(); cand++) {
+			choose[idx] = cand;
+			pro(idx + 1);
+		}
+	}
+	
+	static int solution() {
+		int sum = 0;
+		for(int i=0; i<houses.size(); i++) {
+			int hx = houses.get(i).x;
+			int hy = houses.get(i).y;
+			int dist = Integer.MAX_VALUE;
+			for(int j=0; j<choose.length; j++) {
+				int distance = Math.abs(chickens.get(choose[j]).x - hx) + Math.abs(chickens.get(choose[j]).y - hy);
+				dist = Math.min(dist, distance);
+			}
+			sum += dist;
+		}
+		return sum;
 	}
 
 }
